b1f3988c170f3472739ed38cc40f13fc7e4e8ada
Alexandre Angrignon <alex040892@gmail.com>
2024-05-08 13:12:35 +1200

    Add Routes Controller

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.send('Hello');
 });
 
+//Routes
+app.use('/api/users', require('./routes/UserRoutes'));
+app.use('/api/users/login', require('./routes/UserRoutes'));
+
 app.listen(PORT, () =>
   console.log(colors.cyan(`Server started on port ${PORT}`))
 );
